aa3c3ad94bde488b73a4ec46bf2debbc662fb996
Ziad-Assem648 <ahmdzyadasm@gmail.com>
2025-12-04 23:53:42 +0200

    Update basePath and assetPrefix for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+const isProd = process.env.NODE_ENV === "production";
 
-export default nextConfig
+const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
+  
+  // مهم جدًا لتصدير Static HTML
+  output: "export",
+
+  // اسم الريبو على GitHub Pages
+  basePath: isProd ? "/flutter-basics-website" : "",
+  assetPrefix: isProd ? "/flutter-basics-website/" : "",
+};
+
+export default nextConfig;
